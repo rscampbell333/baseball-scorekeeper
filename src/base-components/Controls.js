bf1c89a27d12a5ddce2acc775f5df5d405b62a4c
rscampbell333 <1151216+rscampbell333@users.noreply.github.com>
2019-10-23 20:48:26 -0500

    prevent scroll bar from covering buttons

--- src/base-components/Controls.js
+++ src/base-components/Controls.js
@@ -3,7 +3,9 @@ import './Controls.css';
 
 export const Controls = ({onSave, onReload}) => {
     return <div className='controls'>
-        <button onClick={onSave}>Save</button>
-        <button onClick={onReload}>Load</button>
+        <div className='buttons'>
+            <button onClick={onSave}>Save</button>
+            <button onClick={onReload}>Load</button>
+        </div>
     </div>
 }
