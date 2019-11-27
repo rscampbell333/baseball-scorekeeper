1bf102f87be1d25827decfa0343a0181e21995ab
rscampbell333 <1151216+rscampbell333@users.noreply.github.com>
2019-11-27 10:40:38 -0600

    make since feature work

--- src/PlayerName.js
+++ src/PlayerName.js
@@ -1,18 +1,18 @@
 import React, { useState, useEffect } from 'react';
 import { EditableText, Dropdown } from './base-components';
 
-export const PlayerName = ({ initName = "", onChange, showInningSelection, id }) => {
+export const PlayerName = ({ initName = "", initSince = 1, onChange, showInningSelection, id }) => {
     const [ name, setName ] = useState(initName);
-    const [ since, setSince ] = useState(1);
+    const [ since, setSince ] = useState(initSince);
 
     useEffect(() => {
-        if(onChange) onChange({ name, since }, id)
+        if(onChange) onChange({ name, since: parseInt(since) }, id)
     }, [ name, since, onChange, id ]);
 
     useEffect(() => setName(initName), [initName, setName]);
 
     return <div className="player dotted-border-bottom">
         <EditableText value={name} onChange={setName} noValueClassName=""/>
-        { showInningSelection && <Dropdown onChange={setSince} options={Array.from({length: 9}, (v, k) => k+1)}/> }
+        { showInningSelection && <Dropdown value={initSince} onChange={setSince} options={Array.from({length: 9}, (v, k) => k+1)}/> }
     </div>
 }
--- src/PlayerNames.js
+++ src/PlayerNames.js
@@ -6,7 +6,8 @@ export const PlayerNames = ({players = [], onChange}) => {
 
     const handlePlayerUpdate = (player, id) => {
         //only store and update if there is a value
-        if(player.name && player.name !== "" && (id === players.length || player.name !== players[id].name)) {
+        if(id === players.length || (player.name  && player.name !== players[id].name)
+            || (player.since !== players[id].since)) {
             const newPlayers = [...players];
 
             if(id < players.length) {
@@ -22,7 +23,7 @@ export const PlayerNames = ({players = [], onChange}) => {
     }
 
     const playerComponents = players.map((player, index) => (
-        <PlayerName initName={player.name} showInningSelection={index > 0} id={index} key={index.toString()} onChange={handlePlayerUpdate}/>
+        <PlayerName initName={player.name} initSince={player.since} showInningSelection={index > 0} id={index} key={index.toString()} onChange={handlePlayerUpdate}/>
     ));
 
     playerComponents.push(<PlayerName id={players.length} showInningSelection={players.length > 0} key={players.length.toString() } onChange={handlePlayerUpdate}/>);
