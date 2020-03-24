92ce5ae4946d96556051d7a9e465cd28860035e2
rscampbell333 <1151216+rscampbell333@users.noreply.github.com>
2020-03-23 20:32:45 -0500

    initial form

--- src/PlayForm.js
+++ src/PlayForm.js
@@ -1,7 +1,43 @@
 import React, { useState } from 'react';
+import { EditableText } from './base-components/EditableText';
+import './PlayForm.css';
 
-const PlayForm = ({result = {}}) => {
-  const { play, fielders } = result;
+const plays = [
+  '1B',
+  '2B',
+  '3B',
+  'HR',
+  'BB',
+  'IBB',
+  'HBP',
+  'K',
+  'ꓘ',
+  'DP',
+  'U',
+  'P',
+  'L',
+  'F',
+  'SAC',
+  'SF',
+  'FC',
+  'E'
+];
 
+export const PlayForm = ({result = {}}) => {
+  const [ fielders, setFielders ] = useState(result.fielders || '');
+  const [ play, setPlay ] = useState(result.play); 
 
+  const handlePlayChange = (play) => setPlay(play);
+  const handleFielderChange = () => {};
+
+  return <div className="play-form">
+    <div className="play">
+      <select value={play}>
+        {plays.map(play => <option value={play} key={play}>{play}</option>)}
+      </select>
+    </div>
+    <div className="fielders">
+      <EditableText onChange={handleFielderChange} placeholder="result" value={fielders}/>
+    </div>
+  </div>;
 }
---
+++ src/PlayForm.stories.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { PlayForm } from './PlayForm';
+
+export default {
+  component: PlayForm,
+  title: 'Play Form'
+};
+
+export const Default = () => <PlayForm/>;
